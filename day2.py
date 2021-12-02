a2d7067185d8d3458144b6a10e65a17a065c7a83
Adrian McGuinnes <adrian.m.mcg@gmail.com>
2021-12-02 18:08:13 +0000

    day 2 part 2

--- day2.py
+++ day2.py
@@ -1,6 +1,6 @@
+with open('./input/day2.txt') as fin:
+    input = [str(i) for i in fin.read().strip().split("\n")]
 def part1():
-    with open('./input/day2.txt') as fin:
-        input = [str(i) for i in fin.read().strip().split("\n")]
     y = 0
     x = 0
     for i in input:
@@ -15,3 +15,21 @@ def part1():
     print(x*y)
 
 part1()
+
+def part2():
+    y = 0
+    x = 0
+    a = 0
+    for i in input:
+        arr = i.split(' ')
+        move = int(arr[1])
+        if arr[0] == 'forward':
+            x += move
+            y += (move * a)
+        elif arr[0] == 'down':
+            a += move
+        elif arr[0] == 'up':
+            a -= move
+    print(x*y)
+    
+part2()
